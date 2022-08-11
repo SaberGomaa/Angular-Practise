dcc0f6960b4d6da12e4ff908617d128b5460b04f
Saber Gomaa <sabergomaa261@gmail.com>
2022-08-11 08:46:53 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({  
+  selector: 'root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
