5c52940bd3d48fc6cb78f1b083e7efa6534d859d
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-15 00:03:29 +0200

    interface

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IProduct {
+  Id:number
+  Name : string
+  Quantity : number
+  Price : number
+}
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent{
+
+
   name:string = 'saber';
   age = 11;
+
+  product : IProduct[] =[
+    {Id : 1 , Name : 'meet' , Price : 20 ,Quantity : 5},
+  ]
 
   lang = ['arbaic','english','german'];
 
