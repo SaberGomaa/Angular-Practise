921d0c6e15943c9348e0ecaeeb088f686d556d56
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-14 21:57:50 +0200

    SERV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
-@Component({  
+@Component({
   selector: 'root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'itiProject Name';
+   name = "saber"
 }
+
+
