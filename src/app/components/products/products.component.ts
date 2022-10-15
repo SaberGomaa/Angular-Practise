dd0df04ea8b55d597c6d188666f683f5b89fa7ee
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-15 11:03:06 +0200

    class & style Binding

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -8,12 +8,13 @@ import { IProduct } from 'src/app/Models/iproduct';
 })
 export class ProductsComponent implements OnInit {
 
+
   proList : IProduct[] ;
 
   constructor() { 
     this.proList = [
       {Id : 1 , Name : 'p1' , Price : 20 ,Quantity : 5 , categoryId : 1 , imgUrl :"https://avatars.githubusercontent.com/u/75697438?v=4"},
-      {Id : 2 , Name : 'p2' , Price : 20 ,Quantity : 5, categoryId : 1 , imgUrl :"https://avatars.githubusercontent.com/u/75697438?v=4"} ,
+      {Id : 2 , Name : 'p2' , Price : 20 ,Quantity : 0, categoryId : 1 , imgUrl :"https://avatars.githubusercontent.com/u/75697438?v=4"} ,
       {Id : 4 , Name : 'p3' , Price : 20 ,Quantity : 25, categoryId : 1 , imgUrl :"https://avatars.githubusercontent.com/u/75697438?v=4"} 
     ]
   }
