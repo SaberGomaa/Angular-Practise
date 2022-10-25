c1e422b4babd02b0ff90dc626767c2614859e946
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-25 20:16:29 +0200

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { OrderMasterComponent } from './components/order-master/order-master.component';
+import { NotFoundComponent } from './components/NotFound/NotFound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // First Match wins strategy
+  //Default path
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Products', component: ProductsComponent },
+  { path: 'Order', component: OrderMasterComponent },
+
+  // Wild-card path
+  { path: '**', component: NotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
