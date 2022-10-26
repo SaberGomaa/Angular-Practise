9fffb0486c90953049e626a844aa938b4151fa1b
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-26 21:15:05 +0200

    rout children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -7,13 +7,16 @@ import { NotFoundComponent } from './components/NotFound/NotFound.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 const routes: Routes = [
   // First Match wins strategy
-  //Default path
-  { path: '', redirectTo: '/Main', pathMatch: 'full' },
+
+  { path: '', component: MainLayoutComponent, children:[
+     //Default path
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
-  { path: 'Main', component: MainLayoutComponent },
   { path: 'Products', component: ProductsComponent },
   { path: 'Order', component: OrderMasterComponent },
 
+  ]},
+ 
   // Wild-card path
   { path: '**', component: NotFoundComponent },
 
