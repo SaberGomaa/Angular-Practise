5f971a9df1eb8cfcd199411210b337debd614703
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-30 15:53:52 +0200

    observable Pips

--- src/app/services/products-api.service.ts
+++ src/app/services/products-api.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry } from 'rxjs';
 import { IProduct } from 'src/app/Models/iproduct';
+import { environment } from './../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +12,25 @@ export class ProductsAPIService {
   constructor(private httpClient: HttpClient) { }
 
   getAllProducts(): Observable<IProduct[]> {
-    return this.httpClient.get<IProduct[]>('http://localhost:3000/products');
+    return this.httpClient.get<IProduct[]>(`${environment.baseAddressForAPI}/products`);
   }
 
   getProductByCatID(catId: number): Observable<IProduct[]> {
     if (catId == 0) {
-      return this.httpClient.get<IProduct[]>('http://localhost:3000/products');
+      return this.httpClient.get<IProduct[]>(`${environment.baseAddressForAPI}/products`);
+      // .pipe(
+      //   retry(3),
+      //   catchError((err)=>{})
+      // );
 
     }
     else {
       // return this.httpClient.get<IProduct[]>('http://localhost:3000/products/?categoryId='+catId) ;
-      return this.httpClient.get<IProduct[]>(`http://localhost:3000/products/?categoryId=${catId}`);
+      return this.httpClient.get<IProduct[]>(`${environment.baseAddressForAPI}/products/?categoryId=${catId}`);
     }
   }
   getProductByID(id : number) :Observable<IProduct>{
-    return this.httpClient.get<IProduct>(`http://localhost:3000/products/${id}`);
+    return this.httpClient.get<IProduct>(`${environment.baseAddressForAPI}/products/${id}`);
   }
 
   addNewProduct(newPrd: IProduct) {
