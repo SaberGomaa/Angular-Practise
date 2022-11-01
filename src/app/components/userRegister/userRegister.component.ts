7c6b88d24e8bc48d553c528869bf96c6ba7da677
sabegomaa261 <sabergomaa261@gmail.com>
2022-11-01 12:57:38 +0200

    set values to form

--- src/app/components/userRegister/userRegister.component.ts
+++ src/app/components/userRegister/userRegister.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
    selector: 'app-userRegister',
@@ -11,24 +11,58 @@ export class UserRegisterComponent implements OnInit {
    UserRegisterFormGroup: FormGroup;
 
 
-   constructor() {
-      this.UserRegisterFormGroup = new FormGroup({
-         name: new FormControl('', [Validators.required, Validators.minLength(10)]),
-         email: new FormControl('', [Validators.email, Validators.required]),
-         mobile: new FormControl(''),
-         address: new FormGroup({
-            street: new FormControl(''),
-            postalCode: new FormControl('')
+   constructor(private FB: FormBuilder) {
+      // this.UserRegisterFormGroup = new FormGroup({
+      //    name: new FormControl('', [Validators.required, Validators.minLength(10)]),
+      //    email: new FormControl('', [Validators.email, Validators.required]),
+      //    mobile: new FormControl(''),
+      //    address: new FormGroup({
+      //       street: new FormControl(''),
+      //       postalCode: new FormControl('')
+      //    }),
+      //    password: new FormControl(''),
+      //    confirmPassword: new FormControl(''),
+      //    reachedBy: new FormControl(''),
+      //    reachedByOther: new FormControl('')
+      // });
+
+      this.UserRegisterFormGroup = FB.group({
+         name: ['', [Validators.required, Validators.minLength(10)]],
+         email: ['', [Validators.email, Validators.required]],
+         mobile: [''],
+         address: FB.group({
+            street: [''],
+            postalCode: [''],
          }),
-         password: new FormControl(''),
-         confirmPassword: new FormControl(''),
-         reachedBy: new FormControl(''),
-         reachedByOther: new FormControl('')
+         password: [''],
+         confirmPassword: [''],
+         reachedBy: [''],
+         reachedByOther: [''],
       });
 
    }
 
    ngOnInit() {
+      /*
+      check URL Params
+      If Find user Id bring user data 
+      else show empty Form to register
+      */
+
+      this.UserRegisterFormGroup.setValue({
+         "name": "sabergomaa",
+         "email": "sabergomaa261@gmail.com",
+         "mobile": "01145595276",
+         "address": {
+            "street": "EGYPT",
+            "postalCode": "1151"
+         },
+         "password": "",
+         "confirmPassword": "",
+         "reachedBy": "FB", 
+         "reachedByOther": "NaN"
+      });
+
    }
 
 
@@ -40,7 +74,7 @@ export class UserRegisterComponent implements OnInit {
       return this.UserRegisterFormGroup.controls['email'];
    }
    register() {
-
+      // call API send {UserRegisterFormGroup.value};
    }
 
 }
